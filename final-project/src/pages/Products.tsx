9d59306da8fdbb5e122dabce1e3d251e66963950
21520703-NTDat <21520703@gm.uit.edu.vn>
2024-10-02 13:41:49 +0700

    FinalS2

--- final-project/src/pages/Products.tsx
+++ final-project/src/pages/Products.tsx
@@ -4,8 +4,15 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TableBody,
+  Button,
+  IconButton,
+  Paper,
 } from "@mui/material";
-interface product {
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+interface Product {
   id: number;
   name: string;
   available: number;
@@ -14,10 +21,58 @@ interface product {
   colors: string;
   price: number;
 }
-const productData = [];
+
+const productData: Product[] = [
+  {
+    id: 1,
+    name: "New name 2",
+    available: 5,
+    sold: 0,
+    category: "cloth",
+    colors: "Blue",
+    price: 459.0,
+  },
+  {
+    id: 2,
+    name: "Backpack Elsassss",
+    available: 1,
+    sold: 0,
+    category: "cloth",
+    colors: "Blue",
+    price: 370.0,
+  },
+  {
+    id: 3,
+    name: "2343",
+    available: 1,
+    sold: 0,
+    category: "Accessory",
+    colors: "Blue",
+    price: 50000.324,
+  },
+  {
+    id: 4,
+    name: "3243",
+    available: 1324,
+    sold: 0,
+    category: "Accessory",
+    colors: "Blue",
+    price: 50000,
+  },
+  {
+    id: 5,
+    name: "Test 2222",
+    available: 10,
+    sold: 0,
+    category: "Accessory",
+    colors: "Blue",
+    price: 55.0,
+  },
+];
+
 const Products = () => {
   return (
-    <TableContainer>
+    <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
@@ -31,8 +86,30 @@ const Products = () => {
             <TableCell>Action</TableCell>
           </TableRow>
         </TableHead>
+        <TableBody>
+          {productData.map((product, index) => (
+            <TableRow key={product.id}>
+              <TableCell>{index + 1}</TableCell>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.available}</TableCell>
+              <TableCell>{product.sold}</TableCell>
+              <TableCell>{product.category}</TableCell>
+              <TableCell>{product.colors}</TableCell>
+              <TableCell>{product.price}</TableCell>
+              <TableCell>
+                <IconButton color="primary">
+                  <EditIcon />
+                </IconButton>
+                <IconButton color="error">
+                  <DeleteIcon />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
       </Table>
     </TableContainer>
   );
 };
+
 export default Products;
